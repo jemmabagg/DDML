ea19e4bf867a06aac12e630b5fd198a0debff24e
Jemma Bagg <jbagg28@gmail.com>
2026-08-26 14:00:31 +0200

    Scaling

--- python/examples/cc3_5_sf_2a_wrapper.py
+++ python/examples/cc3_5_sf_2a_wrapper.py
@@ -181,7 +181,8 @@ def run_inference(inputs):
 
     #post processing (from pcFM)
     pcfm_out = torch.clamp(pcfm_out, min=0.0) # clamp >= 0
-    counts_t = (pcfm_out[:, :n_layers] * 0.64 + 0.5).to(torch.int32)   # CC3 count calibration
+    counts_t = (pcfm_out[:, :n_layers] + 0.5).to(torch.int32)   # CC3 count calibration
+    counts_t *= 0.64
     energy_t = pcfm_out[:, n_layers:2 * n_layers].clone() 
     energy_t[counts_t == 0] = 0.0 #zero the energy per layer where count == 0
 
